4f2f70e8468796c1c56f343891e7b0e0cb2bac87
James <2509941b@student.gla.ac.uk>
2022-10-13 18:30:02 +0100

    Added basic website structure, links, and changing color background

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import React from 'react';
+import Navbar from './components/navbar';
+import Index from './components'
+import About from './components/about';
+import Contact from './components/contact'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+       <Router>
+           <div className="App">
+            <div className="Container">
+              <div id="IndexLink">
+                <Link to="/">Home</Link>
+              </div>
+              <div id="ContactLink">
+                <Link to="/about">Contact</Link>
+              </div>
+              <div id="AboutLink">
+                <Link to="/contact">About</Link>
+              </div>
+            </div>
+            <Routes>
+                  <Route exact path='/' element={< Index />}></Route>
+                  <Route exact path='/about' element={< About />}></Route>
+                  <Route exact path='/contact' element={< Contact />}></Route>
+            </Routes>
+          </div>
+       </Router>
+   );
+  }
 }
-
+  
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './index.css'
+
+function Index() {
+    return (
+        <>
+            <h2>
+                Hi I'm James
+            </h2>
+            <div id="IndexPara">
+                <p>
+                    I'm a third year computer science student, here you can find out about any projects I've worked on or am currently working on.
+                    I am mainly interested in trading and botting, however I have also recently taken an interest in data science.
+                    You can find a summary of projects I'm currently working on here.
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default Index;
